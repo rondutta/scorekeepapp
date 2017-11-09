df13ee8005c9ed0acdbc91ab03ac4dc689606e6f
Rayan <rondutta23@gmail.com>
2017-11-09 17:46:14 +0000

    first commit

---
+++ imports/utils.js
@@ -0,0 +1,5 @@
+console.log('log from /imports/utils.js');
+
+export let greetUser = function(){
+    return 'Hello User !';
+};
---
+++ server/main.js
@@ -0,0 +1,5 @@
+//import './../imports/utils';
+
+import {greetUser} from './../imports/utils';
+console.log('log from /server/main.js');
+console.log(greetUser());
