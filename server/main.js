465f90cff43844f047d41384a99b4d93de31b36b
Rayan <rondutta23@gmail.com>
2017-11-12 20:49:55 +0000

    Class Definitions

--- server/main.js
+++ server/main.js
@@ -2,6 +2,37 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
  Meteor.startup(() => {
-    console.log("Server Running"); 
+    class Person {
+        constructor(name = 'Anonymous', age = 0){
+            this.name = name;
+            this.age = age;
+
+        }
+
+        getGreeting() {
+
+           return `Hi! I am ${this.name}.`; 
+        }
+
+        getPersonDescription() {
+
+            return `${this.name} is ${this.age} year(s) old.`;
+
+        }
+
+    }
+
+    class Employee extends Person {
+        constructor(name, age, title){
+
+            super(name,age);
+            this.title = title ;
+
+        }
+    }
+
+    let me = new Person();
+    let you = new Person('Rayan',21);
+    console.log(you.getPersonDescription());
 });
  
