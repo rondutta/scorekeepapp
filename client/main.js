c2aea2efc3a2831a60a091abbdbf524b69976078
Rayan <rondutta23@gmail.com>
2017-11-12 03:05:14 +0000

    Added from DB.

--- client/main.js
+++ client/main.js
@@ -6,22 +6,22 @@ const players=[{
   _id : '1',  
   name : 'Rayan',
   score : 23
-},
-{
+}, {
   _id : '2',  
   name : 'Sakshi',
   score : 11
 
-},
-{
+}, {
   _id : '3',  
   name : 'Adwi',
   score : -1
   
 }];
 
-const renderPlayer = function() {
-
+const renderPlayers = function(playersList) {
+  return playersList.map(function(player) {
+    return <p key={player._id}>{player.name} has {player.score} point(s)</p>;
+  });
 };
 
 Meteor.startup(function(){
@@ -32,7 +32,7 @@ Meteor.startup(function(){
     <h1>{title}</h1>
     <p>Hello {name}!</p>
     <p>This is my second para.</p>
-    {[<p key='1'>1</p>,<p key='2'>2</p>,<p key='3'>3</p>]}
+    {renderPlayers(players)}
   </div>);
   ReactDOM.render(jsx,document.getElementById('app'));
 });
