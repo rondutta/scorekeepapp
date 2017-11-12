f5ff9e331daa8e8d1a96c299baa9dd5a89ada53a
Rayan <rondutta23@gmail.com>
2017-11-12 01:57:13 +0000

    Added jsx

--- client/main.js
+++ client/main.js
@@ -2,8 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
+const players=[{
+  _id : '1',  
+  name : 'Rayan',
+  score : 23
+},
+{
+  _id : '2',  
+  name : 'Sakshi',
+  score : 11
+
+},
+{
+  _id : '3',  
+  name : 'Adwi',
+  score : -1
+  
+}];
+
+const renderPlayer = function() {
+
+};
+
 Meteor.startup(function(){
   let name = 'Rayan';
-  let jsx = <p>Hello {name}!</p>;
+  let title = 'Score Keep';
+  let jsx = (
+  <div>
+    <h1>{title}</h1>
+    <p>Hello {name}!</p>
+    <p>This is my second para.</p>
+    {[<p key='1'>1</p>,<p key='2'>2</p>,<p key='3'>3</p>]}
+  </div>);
   ReactDOM.render(jsx,document.getElementById('app'));
 });
